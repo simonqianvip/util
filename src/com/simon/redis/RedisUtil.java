e35718d73e8f4a5ce01ad7514c18f0d2cd6bf25f
simonqianvip <simon@simon-THINK>
2015-12-07 15:59:55 +0800

    添加redis util工具类

--- src/com/simon/redis/RedisUtil.java
+++ src/com/simon/redis/RedisUtil.java
@@ -1,73 +1,65 @@
 package com.simon.redis;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisUtil {
 	private static Log log = LogFactory.getLog(RedisUtil.class);
-	private static String ADDR = "172.16.0.147";
-	private static int PORT = 6379;
-	private static int MAX_ACTIVE = 1024;
-	private static int MAX_IDLE = 200;
-	private static int MAX_WAIT = 10000;
-	private static int TIMEOUT = 10000;
-	private static boolean TEST_ON_BORROW = true;
-	private static JedisPool jedisPool = null;
-	/**
-	 * redis init
+	private static Jedis jedis;
+	/*
+	 * 正则规则
 	 */
-	static {
-		try {
-			JedisPoolConfig config = new JedisPoolConfig();
-			config.setMaxTotal(MAX_ACTIVE);
-			config.setMaxIdle(MAX_IDLE);
-			config.setMaxWaitMillis(MAX_WAIT);
-			config.setTestOnBorrow(TEST_ON_BORROW);
-			jedisPool = new JedisPool(config, ADDR, PORT, TIMEOUT);
-			log.info("【创建jedis连接池】");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
+	private static final String REGEX ="[0-9]*";
 	/**
-	 * get redis connectionPool
-	 * 
-	 * @return
-	 */
-	public synchronized static Jedis getJedis() {
-		try {
-			if (jedisPool != null) {
-				Jedis resource = jedisPool.getResource();
-				log.info("【获取jedis实例】");
-				return resource;
-			} else {
-				return null;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	/**
-	 * close redis connection
-	 * 
+	 * 显示库里总数
 	 * @param jedis
 	 */
-	public static void returnResource(final Jedis jedis) {
-		if (jedis != null) {
-			jedisPool.returnResource(jedis);
-			log.info("【释放jedis实例】");
+	public static void showResult(Jedis jedis) {
+		Long size = jedis.dbSize();
+		log.info("【key的总数】：" + size);
+		if(size>=1){
+			log.info("【显示redis库里的所有key】");
+			Set<String> keys = jedis.keys("*");
+			Iterator<String> it = keys.iterator();
+			while (it.hasNext()) {
+				String key = it.next();
+				matcher(key,REGEX);
+			}
 		}
 	}
-	public static void main(String[] args) {
-		RedisUtil.getJedis();
+	/**
+	 * 返回匹配的字符串，否则返回空
+	 * [0-9]*
+	 * @param key
+	 */
+	public static String matcher(String key,String regex) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher isNum = pattern.matcher(key);
+		if(isNum.matches()) {
+			log.info(key);
+			return key;
+		}
+		return "";
+	}
+	/**
+	 * 根据键查找对应的map
+	 * @param key
+	 */
+	private static void showMap(String key) {
+		Map<String, String> map = jedis.hgetAll(key);
+		for (Entry<String, String> entry : map.entrySet()) {
+			log.info("{" + entry.getKey() + " = "
+					+ entry.getValue()+" }");
+		}
 	}
 
 }
